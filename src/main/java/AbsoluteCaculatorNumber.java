4f641c7a841abc654470a8eed082718c10a24aef
longhoang94 <longsutls@gmail.com>
2018-08-15 11:16:54 +0700

    add method

---
+++ src/main/java/AbsoluteCaculatorNumber.java
@@ -0,0 +1,10 @@
+public class AbsoluteCaculatorNumber {
+    public static void main(String[] args) {
+    }
+
+    public static int findAbsolute(int number) {
+        if (number < 0) {
+            return -number;
+        } else return number;
+    }
+}
